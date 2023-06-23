f67954f2ab56d23e9c8f22600d8dbc93deb8dd2a
OmerHerera <omer.herera@gmail.com>
2023-06-23 12:22:29 +0300

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { gql, GraphQLClient } from 'graphql-request';
+
+@Injectable()
+export class AppService {
+  
+  async fetchData(query) {
+    try {
+      const client = new GraphQLClient('http://localhost:3000/graphql');
+      const data = await client.request(query);
+      console.log(data);
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async getHello(): Promise<string> {
+    // Define the GraphQL query
+    const query = gql`
+    query {
+      emoji(code: "1F600") {
+        char
+        name
+        category
+        group
+        subgroup
+      }
+    }
+    `;
+    const data = await this.fetchData(query);
+    return JSON.stringify(data);
+  }
+}
